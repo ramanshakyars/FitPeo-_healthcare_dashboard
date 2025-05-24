e3b21e6feb8fed52ce870421003e4991f74d052f
ramanshakyars <ramanshakyars12@gmail.com>
2025-05-25 01:48:00 +0530

    start working on side navbar

---
+++ src/components/SideNavbar.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import './SideNavbar.css';
+
+function SideNavbar() {
+
+    const [menuItems, setMenuItems] = useState([]);
+
+
+    useEffect(() => {
+        fetch("/SideNavbar.json")
+            .then(response => response.json())
+            .then(data => setMenuItems(data))
+            .catch(error => console.error("Fetch this error", error)
+            )     
+    }, []);
+
+    return (
+        <div className="sidenav">
+            <h2>Side Navbar</h2>
+            <ul>
+                {menuItems.map((item, index) => (
+                    <li key={index}>
+                        <a href={item.link}>{item.title}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default SideNavbar;
